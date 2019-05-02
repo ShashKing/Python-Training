c344f91270a6447b71c753f65ac07f2732cbf871
ShashKing <dngrsshashank@gmail.com>
2019-05-02 16:35:20 +0530

    Class and Objects in python

---
+++ Python Test/ClassAndObject.py
@@ -0,0 +1,15 @@
+class Shashank:
+	a=20
+	b=10
+	def add(self):
+		self.a+=1
+		print(self.a)
+	def sub(self):
+		self.b-=1
+		print(self.b)
+obj = Shashank()
+obj.add()
+obj.sub()
+obj.sub()
+obj1 = Shashank()
+obj1.add()
