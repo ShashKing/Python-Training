cc192a690be6829cf4e6c9b7d97ddfd647d20cc6
ShashKing <dngrsshashank@gmail.com>
2019-05-02 09:52:10 +0530

    If else statement

---
+++ Python Test/Ifelse.py
@@ -0,0 +1,15 @@
+a=20
+if a>20:
+	print("You will get a new bike")
+else:
+	print("You will not get anything")
+
+b = "Neha"
+if b is "Shashank":
+	print("Hello Shashank")
+elif b is "Neha":
+	print("Hello Neha")
+elif b is "Sneha":
+	print("Hello Sneha")
+else:
+	print("No one knows you")
